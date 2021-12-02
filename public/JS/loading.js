fff4c8755f5560ff710700fc480424fff61b93d0
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-12-02 11:32:52 +0100

    %added

--- public/JS/loading.js
+++ public/JS/loading.js
@@ -26,26 +26,25 @@ function updateProgress() {
     ProgressPercentage = ProgressPercentage + 1;
     if (ProgressPercentage >= 0 && ProgressPercentage <= 30) {
       animationInterval += 1;
-      console.log("passe");
-      document.querySelector(".percentage").textContent = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage + "%";
     } else if (ProgressPercentage > 30 && ProgressPercentage <= 60) {
       animationInterval += 2;
       console.log("passe2");
-      document.querySelector(".percentage").textContent = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage + "%";
     } else if (ProgressPercentage > 60 && ProgressPercentage <= 80) {
       animationInterval += 3;
-      document.querySelector(".percentage").textContent = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage + "%";
     } else if (ProgressPercentage > 80 && ProgressPercentage <= 90) {
       animationInterval += 4;
-      document.querySelector(".percentage").textContent = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage + "%";
     } else if (ProgressPercentage > 90 && ProgressPercentage <= 95) {
       animationInterval += 80;
-      document.querySelector(".percentage").textContent = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage + "%";
     } else if (ProgressPercentage > 95 && ProgressPercentage <= 99) {
       animationInterval += 150;
-      document.querySelector(".percentage").textContent = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage + "%";
     } else if (ProgressPercentage >= 100) {
-      document.querySelector(".percentage").textContent = 99;
+      document.querySelector(".percentage").textContent = "99%";
     }
     setTimeout(updateProgress, animationInterval);
   }
