8aea71af5f33336488916f7366d59b813f58e2b1
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-12-02 11:17:07 +0100

    spinner loading percentage gone after page loaded

--- public/JS/loading.js
+++ public/JS/loading.js
@@ -13,12 +13,11 @@ window.document.addEventListener(
 );
 
 function updateProgress() {
-  if (pageState == "complete") {
-    document.querySelector("percentage").innerHTML = 100;
-    console.log(cent);
+  if (pageState == "complete" && ProgressPercentage != null) {
+    document.querySelector(".percentage").textContent = "100%";
     setTimeout(() => {
       // Si le pourcentage arrive à 100% alors on efface la page et on remet une autre page
-      document.querySelector("percentage").style.display = "none";
+      document.querySelector(".loading--circle").style.display = "none";
     }, 700);
   } else {
     if (ProgressPercentage == null) {
@@ -27,34 +26,30 @@ function updateProgress() {
     ProgressPercentage = ProgressPercentage + 1;
     if (ProgressPercentage >= 0 && ProgressPercentage <= 30) {
       animationInterval += 1;
-      document.querySelector("percentage").innerHTML = ProgressPercentage;
+      console.log("passe");
+      document.querySelector(".percentage").textContent = ProgressPercentage;
     } else if (ProgressPercentage > 30 && ProgressPercentage <= 60) {
       animationInterval += 2;
-      document.querySelector("percentage").innerHTML = ProgressPercentage;
+      console.log("passe2");
+      document.querySelector(".percentage").textContent = ProgressPercentage;
     } else if (ProgressPercentage > 60 && ProgressPercentage <= 80) {
       animationInterval += 3;
-      document.querySelector("percentage").innerHTML = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage;
     } else if (ProgressPercentage > 80 && ProgressPercentage <= 90) {
       animationInterval += 4;
-      document.querySelector("percentage").innerHTML = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage;
     } else if (ProgressPercentage > 90 && ProgressPercentage <= 95) {
       animationInterval += 80;
-      document.querySelector("percentage").innerHTML = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage;
     } else if (ProgressPercentage > 95 && ProgressPercentage <= 99) {
       animationInterval += 150;
-      document.querySelector("percentage").innerHTML = ProgressPercentage;
+      document.querySelector(".percentage").textContent = ProgressPercentage;
     } else if (ProgressPercentage >= 100) {
-      document.querySelector("percentage").innerHTML = 99;
+      document.querySelector(".percentage").textContent = 99;
     }
     setTimeout(updateProgress, animationInterval);
   }
 }
-// let elementLoadingBox = document.querySelector(".loading--box");
-// let percentageDiv = document.createElement("div");
-// percentageDiv.className = "percentage";
-// let percentageText = document.createTextNode("0%");
-// percentageDiv.appendChild(percentageText);
-// elementLoadingBox.appendChild(percentageDiv);
 
 let intervalObject = setInterval(() => {
   // Si le body different de undefined
