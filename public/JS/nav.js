8d082269dc08a6c4aa12836a08e4a55be9dbb328
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-12-10 22:18:55 +0100

    ul animation open work

--- public/JS/nav.js
+++ public/JS/nav.js
@@ -4,12 +4,15 @@
   let isOpen = false;
   function displayNav() {
     let navDisplay = document.getElementById("navId");
+    let navId = document.querySelector("nav");
     if (isOpen) {
       navDisplay.classList.remove("nav-open");
       navDisplay.classList.add("nav-close");
+      navId.classList.remove("nav-animation");
     } else {
       navDisplay.classList.remove("nav-close");
       navDisplay.classList.add("nav-open");
+      navId.classList.add("nav-animation");
     }
     // isOpen deviens son inverse : false = true, true = false
     isOpen = !isOpen;
