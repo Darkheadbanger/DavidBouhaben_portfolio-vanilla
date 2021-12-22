ea9aa2f5356d38b18f321487e3db1c68dcc0fa9d
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-12-22 12:13:03 +0100

    webfonts error gone and darkmode text change when clicked

--- public/JS/darkmode.js
+++ public/JS/darkmode.js
@@ -1,16 +1,19 @@
 (() => {
   let checkbox = document.getElementById("darkmode");
   let bodyId = document.getElementById("bodyId");
+  let darkmodeTextSombre = document.getElementById("darkmode-Text");
+  let darkmodeTextClaire = document.getElementById("darkmode-Text");
 
-  function darkmode() { 
-      // checked == true
-    if (checkbox.checked) { 
-      console.log(checkbox.checked)
-        bodyId.classList.add("bodyId")
-    }else{
-        bodyId.classList.remove("bodyId")
+  function darkmode() {
+    // checked == true
+    if (checkbox.checked) {
+      console.log(checkbox.checked);
+      bodyId.classList.add("bodyId");
+      darkmodeTextSombre.innerHTML = `<i class="fa-solid fa-circle-half-stroke"></i>Mode sombre`;
+    } else {
+      bodyId.classList.remove("bodyId");
+      darkmodeTextClaire.innerHTML = `<i class="fa-solid fa-circle-half-stroke"></i>Mode claire`;
     }
   }
-
   checkbox.addEventListener("click", darkmode);
 })();
