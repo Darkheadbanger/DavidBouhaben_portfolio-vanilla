f6f7b16ae59b85de9bddad28b160b8f677385785
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-01-06 00:32:45 +0100

    date onload

--- public/JS/date.js
+++ public/JS/date.js
@@ -1,35 +1,37 @@
-// Array pour nom de jours
-let date = new Date();
-let jour = date.getDate();
-let mois = date.getMonth();
-let annee = date.getFullYear();
-const joursNoms = [
-  "Lundi",
-  "Mardi",
-  "Mercredi",
-  "Jeudi",
-  "Vendredi",
-  "Samedi",
-  "Dimanche",
-];
+window.onload = () => {
+  // Array pour nom de jours
+  let date = new Date();
+  let jour = date.getDate();
+  let mois = date.getMonth();
+  let annee = date.getFullYear();
+  const joursNoms = [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche",
+  ];
 
-const moisNoms = [
-  "janvier",
-  "février",
-  "mars",
-  "avril",
-  "mai",
-  "juin",
-  "juillet",
-  "août",
-  "septembre",
-  "octobre",
-  "novembre",
-  "décembre",
-];
+  const moisNoms = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "août",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre",
+  ];
 
-let nomMois = moisNoms[mois];
-let nomJour = joursNoms[jour];
+  let nomMois = moisNoms[mois];
+  let nomJour = joursNoms[jour];
 
-let o = document.querySelector(".currentTime");
-o.innerHTML = nomJour + ", " + jour + " " + nomMois + " " + annee;
+  let o = document.querySelector(".currentTime");
+  o.innerHTML = nomJour + ", " + jour + " " + nomMois + " " + annee;
+};
