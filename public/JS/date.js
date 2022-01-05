ed7f664058d0240901ee3c2aa9425190dec4a3f1
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-01-05 23:59:32 +0100

    add today date

---
+++ public/JS/date.js
@@ -0,0 +1,35 @@
+// Array pour nom de jours
+let date = new Date();
+let jour = date.getDate();
+let mois = date.getMonth();
+let annee = date.getFullYear();
+const joursNoms = [
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+  "Dimanche",
+];
+
+const moisNoms = [
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "août",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre",
+];
+
+let nomMois = moisNoms[mois];
+let nomJour = joursNoms[jour];
+
+let o = document.querySelector(".currentTime");
+o.innerHTML = nomJour + ", " + jour + " " + nomMois + " " + annee;
