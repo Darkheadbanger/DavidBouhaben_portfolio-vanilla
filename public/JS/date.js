f7ef94ce3f1dc94572051d96ee86afc49a9c74ce
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-01-11 22:44:08 +0100

    setinterval in time

--- public/JS/date.js
+++ public/JS/date.js
@@ -1,50 +1,56 @@
 window.addEventListener("load", () => {
   // Array pournom de jours
-  let date = new Date();
-  console.log(date);
+  function clock() {
+    let date = new Date();
+    console.log(date);
 
-  let jour = date.getDate();
-  let jourNom = date.getDay();
-  console.log(jour);
-  let mois = date.getMonth();
-  let annee = date.getFullYear();
-  let heure = date.getHours();
-  let minute = date.getMinutes();
-  // let second = date.getSeconds();
-  const joursNoms = [
-    "Dimanche", //0
-    "Lundi",
-    "Mardi",
-    "Mercredi",
-    "Jeudi",
-    "Vendredi",
-    "Samedi",
-  ];
+    let jour = date.getDate();
+    let jourNom = date.getDay();
+    console.log(jour);
+    let mois = date.getMonth();
+    let annee = date.getFullYear();
+    let heure = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    // let second = date.getSeconds();
+    const joursNoms = [
+      "Dimanche", //0
+      "Lundi",
+      "Mardi",
+      "Mercredi",
+      "Jeudi",
+      "Vendredi",
+      "Samedi",
+    ];
 
-  const moisNoms = [
-    "janvier", //0
-    "février",
-    "mars",
-    "avril",
-    "mai",
-    "juin",
-    "juillet",
-    "août",
-    "septembre",
-    "octobre",
-    "novembre",
-    "décembre",
-  ];
+    const moisNoms = [
+      "janvier", //0
+      "février",
+      "mars",
+      "avril",
+      "mai",
+      "juin",
+      "juillet",
+      "août",
+      "septembre",
+      "octobre",
+      "novembre",
+      "décembre",
+    ];
 
-  let nomMois = moisNoms[mois]; //dans le tableau moisNoms, on nrécupère (a partir de 0) le getmonth récupéré depuis newDate
-  let nomJour = joursNoms[jourNom];
+    let nomMois = moisNoms[mois]; //dans le tableau moisNoms, on nrécupère (a partir de 0) le getmonth récupéré depuis newDate
+    let nomJour = joursNoms[jourNom];
 
-  // let utcShort = { timeZone: "UTC", timeZoneName: "short" };
-  // let GMT = date.toLocaleTimeString("fr", utcShort);
-  // 
-  let currentTime = document.querySelector(".currentTime");
-  currentTime.innerHTML =
-    "<div class='heureCol'>" +
-    `<span>${heure}:${minute} GMT</span>` +
-    `<span> ${nomJour} ${jour} ${nomMois} ${annee}</span>`;
+    // let utcShort = { timeZone: "UTC", timeZoneName: "short" };
+    // let GMT = date.toLocaleTimeString("fr", utcShort);
+    //
+    let currentTime = document.querySelector(".currentTime");
+    currentTime.innerHTML =
+      "<div class='heureCol'>" +
+      `<span>${heure}:${minute}:${second} GMT+2</span>` +
+      `<span> ${nomJour} ${jour} ${nomMois} ${annee}</span>`;
+
+    setInterval(clock, 1000);
+  }
+  clock();
 });
