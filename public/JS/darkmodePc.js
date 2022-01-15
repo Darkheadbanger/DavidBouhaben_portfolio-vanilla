9b473f61484b2b488ab3aabdaa12f89d27c5e09f
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-01-15 19:31:30 +0100

    toggle and darkmode works on localstorage

--- public/JS/darkmodePc.js
+++ public/JS/darkmodePc.js
@@ -1,18 +1,39 @@
 (() => {
-  let checkboxPc = document.querySelector("#darkmode-pc");
-  let bodyDarkPc = document.querySelector("#bodyId");
-  let darkmodeTextSombrePc = document.querySelector("#darkmode-Text-pc");
-  let darkmodeTextClairePc = document.querySelector("#darkmode-Text-pc");
-
-  function darkmodePc() {
-    if (checkboxPc.checked) {
-      bodyDarkPc.classList.add("bodyDark");
-      darkmodeTextSombrePc.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
-    } else {
-      bodyDarkPc.classList.remove("bodyDark");
-      darkmodeTextClairePc.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
+  window.addEventListener("load", () => {
+    let checkboxPc = document.querySelector("#darkmode-pc");
+    let bodyIdPc = document.querySelector("#bodyId");
+    let darkmodeTextSombrePc = document.querySelector("#darkmode-Text-pc");
+    let darkmodeTextClairePc = document.querySelector("#darkmode-Text-pc");
+    if (localStorage.getItem("darkmode") === null) {
+      localStorage.setItem("darkmode", false);
     }
-  }
+    setInterval(() => {
+      checkDarkmode();
+    });
 
-  checkboxPc.addEventListener("click", darkmodePc);
+    function checkDarkmode() {
+      if (checkboxPc.checked) {
+        localStorage.setItem("darkmode", true);
+        bodyIdPc.classList.add("bodyDark");
+        darkmodeTextSombrePc.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
+      } else {
+        localStorage.setItem("darkmode", false);
+        bodyIdPc.classList.remove("bodyDark");
+        darkmodeTextClairePc.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
+      }
+    }
+    function darkmodePc() {
+      if (checkboxPc.checked) {
+        localStorage.setItem("darkmode", true);
+        bodyIdPc.classList.add("bodyDark");
+        darkmodeTextSombrePc.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
+      } else {
+        localStorage.setItem("darkmode", false);
+        bodyIdPc.classList.remove("bodyDark");
+        darkmodeTextClairePc.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
+      }
+    }
+
+    checkboxPc.addEventListener("click", darkmodePc);
+  });
 })();
