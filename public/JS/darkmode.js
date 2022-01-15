b81fce80c5e5f19e798e1059ee4f567a6a0b996e
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-01-15 19:31:35 +0100

    toggle and darkmode works on localstorage

--- public/JS/darkmode.js
+++ public/JS/darkmode.js
@@ -1,26 +1,40 @@
 (() => {
-  let checkbox = document.getElementById("darkmode");
-  let bodyId = document.getElementById("bodyId");
-  let darkmodeTextSombre = document.getElementById("darkmode-Text");
-  let darkmodeTextClaire = document.getElementById("darkmode-Text");
-
-
-  function darkmode() {
-    console.log("darkmode entrée");
-    // checked == true
-    if (checkbox.checked) {
-      // darkmodePc();
-      bodyId.classList.add("bodyDark");
-      darkmodeTextSombre.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
-      // bodyDarkPc.classList.add("bodyDark");
-      // darkmodeTextSombrePc.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
-    } else {
-      bodyId.classList.remove("bodyDark");
-      darkmodeTextClaire.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
-      // bodyDarkPc.classList.remove("bodyDark");
-      // darkmodeTextClairePc.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
+  window.addEventListener("load", (event) => {
+    let checkbox = document.getElementById("darkmode");
+    let bodyId = document.getElementById("bodyId");
+    let darkmodeTextSombre = document.getElementById("darkmode-Text");
+    let darkmodeTextClaire = document.getElementById("darkmode-Text");
+    if (localStorage.getItem("darkmode") === null) {
+      localStorage.setItem("darkmode", false);
     }
-  }
-  checkbox.addEventListener("click", darkmode);
-
+    setInterval(() => {
+      checkDarkmode();
+    });
+    // Checker si le localstorage existe ou non, si il existe alors baclground noir
+    function checkDarkmode() {
+      if (checkbox.checked) {
+        localStorage.setItem("darkmode", true);
+        bodyId.classList.replace("bodyLight", "bodyDark");
+        darkmodeTextSombre.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
+      } else {
+        localStorage.setItem("darkmode", false);
+        // bodyId.classList.remove("bodyDark");
+        darkmodeTextClaire.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
+      }
+    }
+    // Cliquer toggle va faire apparaitre darkmode
+    function darkmode() {
+      // localStorage.setItem("darkmodeMobile", checkbox.checked); // checked == true
+      if (checkbox.checked) {
+        localStorage.setItem("darkmode", true);
+        bodyId.classList.add("bodyDark");
+        darkmodeTextSombre.innerHTML = `<i class="fas fa-lightbulb sombre"></i>Mode sombre`;
+      } else {
+        localStorage.setItem("darkmode", false);
+        bodyId.classList.remove("bodyDark");
+        darkmodeTextClaire.innerHTML = `<i class="far fa-lightbulb claire"></i>Mode claire`;
+      }
+    }
+    checkbox.addEventListener("click", darkmode);
+  });
 })();
