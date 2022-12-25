4a6577dc54c7f85b96ea1fd29f9cc8bdb2889cb3
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-12-25 17:26:23 +0100

    Cours angular

---
+++ Angular-learning/Chapitre 1/beginner/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  // Revoir le DOM et DOM virtuel
+})
+export class AppModule { }
